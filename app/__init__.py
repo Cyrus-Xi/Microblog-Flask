2a0f74b4b7b2953cee107296b6296a8d3947a08e
Cyrus Xi <xi.cyrus@gmail.com>
2015-05-12 21:12:25 -0400

    Add base template

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,6 @@
 from flask import Flask
 
 app = Flask(__name__)  # Create the application object of type Flask.
+app.config.from_object('config')  # Read and use config file.
+
 from app import views  # At the end to avoid a circular import error.
