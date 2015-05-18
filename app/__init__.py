36236f5d1437cb79b9d03ae00dca9c7bfbebf043
Cyrus Xi <xi.cyrus@gmail.com>
2015-05-17 23:15:31 -0400

    Preliminary database set-up

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)  # Create the application object of type Flask.
 app.config.from_object('config')  # Read and use config file.
+db = SQLAlchemy(app)  # Create a db object to be the database.
 
-from app import views  # At the end to avoid a circular import error.
+from app import views, models  # At the end to avoid a circular import error.
